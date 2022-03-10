6c62e47cb82d5b012063ffe6024873569818e42e
Budi-Laptop <budi.salah@outlook.com>
2022-03-10 18:03:24 +0200

    finish functionality for reset btn, next player, and square value

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import React from "react";
+import { useState } from "react";
 import Square from "./Square";
 
 export default function Board() {
@@ -39,30 +39,105 @@ export default function Board() {
     fontSize: "16px",
   };
 
+  const [nextPlayer, setNextPlayer] = useState("x");
+  const [remainingSteps, setRemainingSteps] = useState(9);
+  const [resetValue, setResetValue] = useState(false);
+
+  function resetHandle() {
+    setNextPlayer("x");
+    setRemainingSteps(9);
+    setResetValue((oldVal) => !oldVal);
+  }
+
   return (
     <div style={containerStyle} className="gameBoard">
       <div id="statusArea" className="status" style={instructionsStyle}>
-        Next player: <span>X</span>
+        {remainingSteps === 0 ? (
+          <span style={{ textTransform: "capitalize" }}>game over</span>
+        ) : (
+          <span style={{ textTransform: "capitalize" }}>
+            Next player: {nextPlayer}
+          </span>
+        )}
       </div>
+
       <div id="winnerArea" className="winner" style={instructionsStyle}>
         Winner: <span>None</span>
       </div>
-      <button style={buttonStyle}>Reset</button>
+
+      <button style={buttonStyle} onClick={resetHandle}>
+        Reset
+      </button>
+
       <div style={boardStyle}>
         <div className="board-row" style={rowStyle}>
-          <Square />
-          <Square />
-          <Square />
+          <Square
+            path="1"
+            nextPlayer={nextPlayer}
+            setNextPlayer={setNextPlayer}
+            setRemainingSteps={setRemainingSteps}
+            resetValue={resetValue}
+          />
+          <Square
+            path="2"
+            nextPlayer={nextPlayer}
+            setNextPlayer={setNextPlayer}
+            setRemainingSteps={setRemainingSteps}
+            resetValue={resetValue}
+          />
+          <Square
+            path="3"
+            nextPlayer={nextPlayer}
+            setNextPlayer={setNextPlayer}
+            setRemainingSteps={setRemainingSteps}
+            resetValue={resetValue}
+          />
         </div>
         <div className="board-row" style={rowStyle}>
-          <Square />
-          <Square />
-          <Square />
+          <Square
+            path="4"
+            nextPlayer={nextPlayer}
+            setNextPlayer={setNextPlayer}
+            setRemainingSteps={setRemainingSteps}
+            resetValue={resetValue}
+          />
+          <Square
+            path="5"
+            nextPlayer={nextPlayer}
+            setNextPlayer={setNextPlayer}
+            setRemainingSteps={setRemainingSteps}
+            resetValue={resetValue}
+          />
+          <Square
+            path="6"
+            nextPlayer={nextPlayer}
+            setNextPlayer={setNextPlayer}
+            setRemainingSteps={setRemainingSteps}
+            resetValue={resetValue}
+          />
         </div>
         <div className="board-row" style={rowStyle}>
-          <Square />
-          <Square />
-          <Square />
+          <Square
+            path="7"
+            nextPlayer={nextPlayer}
+            setNextPlayer={setNextPlayer}
+            setRemainingSteps={setRemainingSteps}
+            resetValue={resetValue}
+          />
+          <Square
+            path="8"
+            nextPlayer={nextPlayer}
+            setNextPlayer={setNextPlayer}
+            setRemainingSteps={setRemainingSteps}
+            resetValue={resetValue}
+          />
+          <Square
+            path="9"
+            nextPlayer={nextPlayer}
+            setNextPlayer={setNextPlayer}
+            setRemainingSteps={setRemainingSteps}
+            resetValue={resetValue}
+          />
         </div>
       </div>
     </div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,6 +1,12 @@
-import React from "react";
+import { useState, useEffect } from "react";
 
-export default function Square() {
+export default function Square({
+  path,
+  nextPlayer,
+  setNextPlayer,
+  setRemainingSteps,
+  resetValue,
+}) {
   const squareStyle = {
     width: "60px",
     height: "60px",
@@ -8,10 +14,41 @@ export default function Square() {
     margin: "4px",
     display: "flex",
     justifyContent: "center",
-    alignItems: "center",
-    fontSize: "20px",
-    color: "white",
+    alignItems: "flex-start",
+    fontSize: "42px",
+    color: "#333",
   };
 
-  return <div className="square" style={squareStyle}></div>;
+  const [value, setValue] = useState("");
+
+  function clickHandle({ target }) {
+    console.log(target.getAttribute("data-path"));
+
+    if (!value) {
+      setValue(nextPlayer);
+
+      setNextPlayer((oldVal) => {
+        return String(oldVal).toLowerCase() === "x" ? "o" : "x";
+      });
+
+      setRemainingSteps((oldVal) => {
+        return (oldVal -= 1);
+      });
+    }
+  }
+
+  useEffect(() => {
+    setValue("");
+  }, [resetValue]);
+
+  return (
+    <div
+      onClick={clickHandle}
+      className="square"
+      style={squareStyle}
+      data-path={path}
+    >
+      {value}
+    </div>
+  );
 }
